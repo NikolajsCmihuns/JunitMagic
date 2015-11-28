fb0b7cec8ff0fd457b58b61e3cb4594529921846
Viktors.Savonins <javagurulv@gmail.com>
2015-11-28 15:39:59 +0200

    section 2

--- src/main/java/lv/javaguru/junit/workshop/section2/TaxCalculator.java
+++ src/main/java/lv/javaguru/junit/workshop/section2/TaxCalculator.java
@@ -2,15 +2,9 @@ package lv.javaguru.junit.workshop.section2;
 
 public class TaxCalculator {
 
-    public double calculateTax(double income, int year) {
-        double tax;
-
-        if(year < 2011) {
-            tax = 0.18;
-        }
-        else tax = 0.22;
-
-        return income * tax;
+    public double calculateTax(double income) {
+        // Provide implementation after writing tests!
+        return 0;
     }
 
 }
