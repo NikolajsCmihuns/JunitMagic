c343d36f0bd863df182214494b15950535faf2d1
Viktors.Savonins <javagurulv@gmail.com>
2015-11-28 08:12:33 +0200

    Array sorting tests example

--- src/test/java/lv/javaguru/junit/workshop/section1/ArraySortingTest.java
+++ src/test/java/lv/javaguru/junit/workshop/section1/ArraySortingTest.java
@@ -8,10 +8,33 @@ import static org.junit.Assert.assertThat;
 public class ArraySortingTest {
 
     @Test
-    public void testArrayWithOneElement() {
+    public void whenSortEmptyArrayThenNothingHappens() {
+        int[] arr = {};
+        ArraySorting.sortArray(arr);
+        assertThat(arr.length, is(0));
+    }
+
+    @Test
+    public void whenSortArrayWithOneElementThenNothingHappens() {
         int[] arr = {1};
         ArraySorting.sortArray(arr);
         assertThat(arr[0], is(1));
+    }
+
+    @Test
+    public void whenSortArrayWithTwoElementInCorrectOrderThenOrderNotChanged() {
+        int[] arr = {1, 2};
+        ArraySorting.sortArray(arr);
+        assertThat(arr[0], is(1));
+        assertThat(arr[1], is(2));
+    }
+
+    @Test
+    public void whenSortArrayWithTwoElementWithNotCorrectOrderThenChangeOrder() {
+        int[] arr = {2, 1};
+        ArraySorting.sortArray(arr);
+        assertThat(arr[0], is(1));
+        assertThat(arr[1], is(2));
     }
 
 }
