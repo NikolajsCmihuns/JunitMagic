ee9416d1cb9e3e8d5e3372c50018058746ed54e4
javagurulv <javagurulv@gmail.com>
2015-11-29 07:36:33 +0200

    cucumber reports folder

--- src/test/java/lv/javaguru/junit/workshop/section2_bdd/TaxCalculatorAcceptanceTest.java
+++ src/test/java/lv/javaguru/junit/workshop/section2_bdd/TaxCalculatorAcceptanceTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features/taxCalculator.feature",
-                 format = "html:target/cucumber-html-report",
+                 format = "html:build/reports/cucumber-html-report",
                  glue = "lv.javaguru.junit.workshop.section2_bdd")
 public class TaxCalculatorAcceptanceTest {
 
